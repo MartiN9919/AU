b7c90466f4c74b95656c7b92abef284aad3394b9
Martin <andrey.martsynkevich@gmail.com>
2022-10-17 16:14:03 +0300

    it_table

--- aut/technics/admin.py
+++ aut/technics/admin.py
@@ -1,24 +1,45 @@
 from django.contrib import admin
-from .models import ItTechnicsModels, ModuleOrganization
+from .models import ItTechnicsModels, ModuleOrganization, ItTechnicsType
 
 
-
-@admin.register(ItTechnicsModels)
-class ItTechnicsModels(admin.ModelAdmin):
+@admin.register(ItTechnicsType)
+class ItTechnicsType(admin.ModelAdmin):
     list_display = (
+        'id',
         'name',
-        'name1',
-        'add_date',
-        'categorian',
-        'organization',
-        'img'
+        'description',
     )
 
-    list_filter = ('name','name1', 'categorian', 'add_date','organization',)
-    list_editable = ('categorian', 'add_date','organization','name1',)
+    list_filter = ('name', 'description',)
+    list_per_page = 30
+@admin.register(ItTechnicsModels)
+class ItTechnicsModels(admin.ModelAdmin):
+
+    list_display = (
+        'id',
+        'type',
+        'id_name',
+        'date_start',
+        'date_end',
+        'date_max',
+        'decommissioning_date',
+        'categorian',
+        'organization',
+        'img',
+        'wifi_availability',
+        'date_shutdown',
+        'serial_numbers_device',
+        'number_information',
+        'date_start_Kanoe',
+        'date_end_Kanoe',
+    )
+
+    list_select_related = True
+
+    list_filter = ('type','categorian',)
+    list_editable = ()
     list_per_page = 20
     search_fields = (
-       'add_date',
     )
 
 
@@ -30,6 +51,6 @@ class ModuleOrganization(admin.ModelAdmin):
         'name',
         'description',
     )
-
     list_filter = ('name', 'description',)
     list_per_page = 30
+
--- aut/technics/models.py
+++ aut/technics/models.py
@@ -27,17 +27,59 @@ class ModuleOrganization(models.Model):
         return self.name
 
 
+class ItTechnicsType(models.Model):
+    id = models.AutoField(
+        primary_key=True,
+        verbose_name='id',
+        unique=True,
+    )
+    name= models.CharField(
+        max_length=60,
+        verbose_name='Тип техники',
+    )
 
+    description= models.CharField(
+        max_length=60,
+        verbose_name='Дополнительное описание',
+        blank=True
+
+    )
+
+    def __str__(self):
+        return self.name
 
 class ItTechnicsModels(models.Model):
-    name = models.CharField(
-        max_length=60,
+    id = models.AutoField(
+        primary_key=True,
+        verbose_name='№',
+
+    )
+    type = models.ForeignKey(
+        ItTechnicsType,
+        on_delete=models.CASCADE,
+        db_column='name',
         verbose_name='Наименование Техники',
     )
-    add_date = models.DateField(
-        verbose_name='Дата постановки на учёт',
+    id_name = models.CharField(
+        max_length=60,
+        verbose_name='Инвентарный номер',
     )
-
+    date_start =models.DateField(
+        verbose_name='Дата ввода в эксплуатацию',
+        null=True,
+    )
+    date_end = models.DateField(
+        verbose_name='Дата окончания',
+        null=True,
+    )
+    date_max = models.DurationField(
+        verbose_name='MAX Период эксплуатации',
+        null=True,
+    )
+    decommissioning_date = models.DateField(
+        verbose_name='Дата вывода из эксплуатации',
+        null=True,
+    )
     categorian = models.BooleanField(
         verbose_name='Категорированная',
     )
@@ -48,14 +90,42 @@ class ItTechnicsModels(models.Model):
         null=True,
 
     )
-
-    name1 = models.CharField(
-        max_length=60,
-        verbose_name='Наименование Техники',
+    SIZES = (
+                ('Отсутствует', 'Отсутствует'),
+                ('Изъято', 'Изъято'),
+                ('В наличии/не изымалось', 'В наличии/не изымалось'),
+            )
+    wifi_availability = models.CharField(
+        max_length= 100,
+        choices=SIZES,
     )
+
+    date_shutdown = models.DateField(
+        verbose_name='Дата Изъятия модуля БПД',
+        null=True,
+    )
+
+    serial_numbers_device = models.CharField(
+        max_length=60,
+        verbose_name=' Серийный номер модуля',
+    )
+
     img = models.ImageField(
+        null=True,
         upload_to='products_images',
-        verbose_name='Изображение'
-
-
+        verbose_name='Изображение',
+        blank=True,
      )
+    number_information = models.CharField(
+        max_length= 7,
+        verbose_name='Номер СВТ',
+    )
+
+    date_start_Kanoe = models.DateField(
+        verbose_name='Дата установки ПО КАНОЭ',
+        null=True,
+    )
+    date_end_Kanoe = models.DateField(
+        verbose_name='Дата окончания ПО КАНОЭ',
+        null=True,
+    )
